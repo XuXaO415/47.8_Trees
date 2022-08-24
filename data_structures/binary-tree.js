a93f0e6b69842961ddb0ef90afb2dbd41fd0ca07
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-08-23 18:19:30 -0700

    Added min & max depth methods

--- data_structures/binary-tree.js
+++ data_structures/binary-tree.js
@@ -3,35 +3,66 @@
 class BinaryTreeNode {
   constructor(val, left = null, right = null) {
     this.val = val;
-    this.left = left;
-    this.right = right;
+    this.left = left; // left child node, left node is a leaf node
+    this.right = right; // right child node, right node is a leaf node
+    //Nodes that directly follow the root node are the children of the root node, right???
   }
 }
 
 class BinaryTree {
   constructor(root = null) {
-    this.root = root;
+    this.root = root; // root node, 
   }
+
 
   /** minDepth(): return the minimum depth of the tree -- that is,
    * the length of the shortest path from the root to a leaf. */
 
-  minDepth() {
+  //Given a binary tree, find its minimum depth. 
+  //The minimum depth is the number of nodes along the shortest path from root node down to the nearest leaf node.
 
+  // minDepth() {
+  //   if (!this.root) return 0; // if the tree is empty, return 0
+
+
+  //   function minDepthHelper(node, depth = 1) {
+  //     if (!node.left && !node.right) return depth; // if the node is not a leaf, return the depth
+  //     if (!node.left) return minDepth(node.right, depth + 1); // if the node has no left child, go to the right child
+  //     if (!node.right) return minDepth(node.left, depth + 1); // if the node has no right child, go to the left child
+  //     return Math.min(minDepth(node.left, depth + 1), minDepth(node.right, depth + 1)); // if the node has both children, go to the left child if the left child has a smaller depth, otherwise go to the right child
+  //   }
+  //   console.log(minDepth(this.root));
+  //   return minDepth(this.root);
+  // }
+
+  minDepth(rootNode = this.root, depth = 0) {
+    if (!rootNode) return depth; // if the tree is empty, return 0
+    const leftDepth = this.minDepth(rootNode.left, depth + 1); // leftDepth equals the minimum depth of the left child node 
+    const rightDepth = this.minDepth(rootNode.right, depth + 1); // rightDepth equals the minimum depth of the right child node
+    console.log(leftDepth, rightDepth);
+    return Math.min(leftDepth, rightDepth); // return the minimum depth of the left child node and the right child node
   }
+
 
   /** maxDepth(): return the maximum depth of the tree -- that is,
    * the length of the longest path from the root to a leaf. */
 
-  maxDepth() {
+  maxDepth(rootNode = this.root, depth = 0) {
+    if (!rootNode) return depth; // if the tree is empty, return 0
+    const leftDepth = this.maxDepth(rootNode.left, depth + 1);
+    const rightDepth = this.maxDepth(rootNode.right, depth + 1);
+    return Math.max(leftDepth, rightDepth); // return the maximum depth of the left child node & right child node
 
   }
 
   /** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
    * The path doesn't need to start at the root, but you can't visit a node more than once. */
 
-  maxSum() {
-
+  maxSum(rootNode = this.root) {
+    if (!this.root) return 0; // if the tree is empty, return 0
+    const leftDepth = this.maxSum(this.root.left, 0); // leftDepth equals the maximum sum of the left child node 
+    const rightDepth = this.maxSum(this.root.right, 0); // rightDepth equals the maximum sum of the right child node
+    return Math.max(leftDepth, rightDepth); // return the maximum sum of the left child node & right child node
   }
 
   /** nextLarger(lowerBound): return the smallest value in the tree
@@ -68,8 +99,11 @@ class BinaryTree {
    * of two nodes in a binary tree. */
 
   lowestCommonAncestor(node1, node2) {
-    
+
   }
 }
 
-module.exports = { BinaryTree, BinaryTreeNode };
+module.exports = {
+  BinaryTree,
+  BinaryTreeNode
+};
