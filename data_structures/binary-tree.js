b0ce4259ed78f3ef6464e37939ec73d8f044654f
XuXaO415 <52443523+XuXaO415@users.noreply.github>
2022-08-24 19:35:25 -0700

    Completed assignment

--- data_structures/binary-tree.js
+++ data_structures/binary-tree.js
@@ -2,7 +2,7 @@
 
 class BinaryTreeNode {
   constructor(val, left = null, right = null) {
-    this.val = val;
+    this.val = val; // value of the node
     this.left = left; // left child node, left node is a leaf node
     this.right = right; // right child node, right node is a leaf node
     //Nodes that directly follow the root node are the children of the root node, right???
@@ -39,7 +39,7 @@ class BinaryTree {
     if (!rootNode) return depth; // if the tree is empty, return 0
     const leftDepth = this.minDepth(rootNode.left, depth + 1); // leftDepth equals the minimum depth of the left child node 
     const rightDepth = this.minDepth(rootNode.right, depth + 1); // rightDepth equals the minimum depth of the right child node
-    console.log(leftDepth, rightDepth);
+    // console.log(leftDepth, rightDepth);
     return Math.min(leftDepth, rightDepth); // return the minimum depth of the left child node and the right child node
   }
 
@@ -57,53 +57,161 @@ class BinaryTree {
 
   /** maxSum(): return the maximum sum you can obtain by traveling along a path in the tree.
    * The path doesn't need to start at the root, but you can't visit a node more than once. */
+  //Given a binary tree, find the maximum path sum. 
+  //The path may start and end at any node in the tree, but no node can be visited more than once.
 
-  maxSum(rootNode = this.root) {
-    if (!this.root) return 0; // if the tree is empty, return 0
-    const leftDepth = this.maxSum(this.root.left, 0); // leftDepth equals the maximum sum of the left child node 
-    const rightDepth = this.maxSum(this.root.right, 0); // rightDepth equals the maximum sum of the right child node
-    return Math.max(leftDepth, rightDepth); // return the maximum sum of the left child node & right child node
+  //failed test case for handling complex trees and negative values
+  // maxSum(rootNode = this.root) {
+  //   //base case
+  //   if (!rootNode) return 0;
+
+  //   //recursive case
+  //   let leftSum = this.maxSum(rootNode.left);
+  //   let rightSum = this.maxSum(rootNode.right);
+  //   let maxSum = Math.max(leftSum, rightSum);
+  //   let returnMaxSum = rootNode.val + leftSum + rightSum;
+  //   return Math.max(maxSum, returnMaxSum);
+  // }
+
+  // maxSum() {
+  //   let sum = 0;
+
+  //   function maxSumHelper(node) {
+  //     if (!node) return 0;
+  //     let leftSum = maxSumHelper(node.left);
+  //     let rightSum = maxSumHelper(node.right);
+  //     let maxSum = Math.max(leftSum, rightSum);
+  //     let returnMaxSum = node.val + leftSum + rightSum;
+  //     return Math.max(maxSum, returnMaxSum);
+  //   }
+  //   return maxSumHelper(this.root);
+  // }
+
+  maxSum() {
+    let sum = 0;
+
+    function maxSumHelper(node) {
+      if (!node) return 0; // if the tree is empty, return 0
+      let leftSum = maxSumHelper(node.left); // leftSum equals the maximum sum of the left child node
+      let rightSum = maxSumHelper(node.right); // rightSum equals the maximum sum of the right child node
+      sum = Math.max(sum, node.val + leftSum + rightSum); // sum equals the maximum sum of the left child node and the right child node
+      return Math.max(0, leftSum, rightSum) + node.val; // return the maximum sum of the left child node and the right child node plus the value of the current node
+    }
+    maxSumHelper(this.root); // call the maxSumHelper function
+    return sum;
   }
+
 
   /** nextLarger(lowerBound): return the smallest value in the tree
    * which is larger than lowerBound. Return null if no such value exists. */
 
-  nextLarger(lowerBound) {
+  //Given a binary tree and a integer x. Find and return the value of the node with next larger element in the tree i.e. 
+  //find a node with value just greater than x. Return null if no node is present with value greater than x.
 
+  // nextLarger(lowerBound) {
+  //   // base case
+  //   if (!this.root) return null;
+
+  //   let queue = [this.root]; // create a queue to store the nodes in the tree
+  //   let nextLarger = null; // create a variable to store the next larger value
+
+  //   while (queue.length) { // while the queue is not empty
+  //     let currentNode = queue.shift(); // remove the first node in the queue and assign it to currentNode
+  //     let currentValue = currentNode.val; // assign the value of the current node to currentValue
+  //     let higherThanLowerBound = currentValue > lowerBound; // check if the current value is greater than the lower bound
+  //     let lowerThanNextLarger = currentValue < nextLarger; // check if the current value is less than the next larger value
+  //     if (higherThanLowerBound && lowerThanNextLarger) { // if the current value is greater than the lower bound and less than the next larger value
+  //       nextLarger = currentValue; // assign the current value to the next larger value
+  //     }
+  //     if (currentNode.left) queue.push(currentNode.left); // if the current node has a left child, add/ push the left child to the queue
+  //     if (currentNode.right) queue.push(currentNode.right); // if the current node has a right child, add/ push the right child to the queue
+  //   }
+  //   return nextLarger; // return the next larger value
+  // }
+
+  nextLarger(lowerBound) {
+    if (!this.root) return null;
+
+    // let's use BFS for this!
+    let queue = [this.root];
+    let closest = null;
+
+    while (queue.length) {
+      let currentNode = queue.shift();
+      let currentVal = currentNode.val;
+      let higherThanLowerBound = currentVal > lowerBound;
+      let shouldReassignClosest = currentVal < closest || closest === null; // 
+
+      if (higherThanLowerBound && shouldReassignClosest) {
+        closest = currentVal;
+      }
+
+      if (currentNode.left) queue.push(currentNode.left);
+      if (currentNode.right) queue.push(currentNode.right);
+    }
+
+    return closest;
   }
 
   /** Further study!
    * areCousins(node1, node2): determine whether two nodes are cousins
    * (i.e. are at the same level but have different parents. ) */
 
-  areCousins(node1, node2) {
+  // areCousins(node1, node2) {
 
-  }
+  // }
 
   /** Further study!
    * serialize(tree): serialize the BinaryTree object tree into a string. */
 
-  static serialize() {
+  // static serialize() {
 
-  }
+  // }
 
   /** Further study!
    * deserialize(stringTree): deserialize stringTree into a BinaryTree object. */
 
-  static deserialize() {
+  // static deserialize() {
 
-  }
+  // }
 
   /** Further study!
    * lowestCommonAncestor(node1, node2): find the lowest common ancestor
    * of two nodes in a binary tree. */
 
-  lowestCommonAncestor(node1, node2) {
+  // lowestCommonAncestor(node1, node2) {
 
-  }
+  // }
 }
 
 module.exports = {
   BinaryTree,
   BinaryTreeNode
 };
+
+emptyTree = new BinaryTree();
+
+// build small tree;
+let smallLeft = new BinaryTreeNode(5);
+let smallRight = new BinaryTreeNode(5);
+let smallRoot = new BinaryTreeNode(6, smallLeft, smallRight);
+smallTree = new BinaryTree(smallRoot);
+
+// build large tree
+let node6 = new BinaryTreeNode(1);
+let node5 = new BinaryTreeNode(1);
+let node4 = new BinaryTreeNode(2);
+let node3 = new BinaryTreeNode(3, node4, node6);
+let node2 = new BinaryTreeNode(5, node3, node5);
+let node1 = new BinaryTreeNode(5);
+let root = new BinaryTreeNode(6, node1, node2);
+largeTree = new BinaryTree(root);
+
+console.log("1. Small tree min depth:", smallTree.minDepth(2)); // 3
+console.log("2. Large tree min depth:", largeTree.minDepth(2)); // 3
+console.log("3. Small tree max depth:", smallTree.maxDepth(2)); // 3
+console.log("4. Large tree max depth:", largeTree.maxDepth(2)); // 5
+console.log("5. Small tree max sum:", smallTree.maxSum()); // 16
+console.log("6. Large tree max sum:", largeTree.maxSum()); // 21
+console.log("7. Small tree next larger:", smallTree.nextLarger(5)); // 6
+console.log("8. Large tree next larger:", largeTree.nextLarger(6)); // null
--- data_structures/tree.js
+++ data_structures/tree.js
@@ -13,23 +13,185 @@ class Tree {
   }
 
   /** sumValues(): add up all of the values in the tree. */
+  //Given a n-ary tree of integers, return the sum of all the integers.
+
+  // sumValues(rootNode = this.root, sum = 0) {
+  //   if (!rootNode) return sum; // if the tree is empty, return 0
+  //   let currentNode = rootNode.push(rootNode.val + this.sumValues(rootNode.children, sum));
+  //   return currentNode;
+
+  // }
+
+  // sumValues() {
+  //   let sum = 0;
+
+  //   if (!this.root) {
+  //     return sum;
+  //   } else {
+  //     const queue = [this.root];
+  //     while (queue.length > 0) {
+  //       const currentNode = queue.shift();
+  //       sum += currentNode.val;
+  //       for (let i = 0; i < currentNode.children.length; i++) {
+  //         queue.push(currentNode.children[i]);
+  //       }
+  //     }
+  //     return sum;
+  //   }
+  // }
+
+  // sumValues(sum = 0) {
+  //   if (!this.root) return sum;
+  //   const treeStack = [this.root];
+  //   while (treeStack.length) {
+  //     const currentStack = treeStack.shift();
+  //     if (currentStack.val) { //
+  //       sum += currentStack.val;
+  //     }
+  //     for (let child of currentStack.children) {
+  //       treeStack.push(child);
+  //     }
+  //   }
+
+  //   return sum;
+  // }
+
+  // sumValues(val) {
+  //   if (!this.root) return 0;
+  //   let sum = 0;
+
+  //   let toVisitQueue = [this.root];
+  //   while (toVisitQueue.length) {
+  //     let currentNode = toVisitQueue.shift();
+  //     if (!currentNode) continue;
+  //     sum += currentNode.val;
+  //     for (let i = 0; i < currentNode.children.length; i++) {
+  //       toVisitQueue.push(currentNode.children[i]);
+  //     }
+  //   }
+  //   return sum;
+  // }
+
 
   sumValues() {
-    
+    if (!this.root) return 0;
+    let sum = 0;
+
+    let toVisitQueue = [this.root]; // create a queue to hold the nodes to visit
+    while (toVisitQueue.length) {
+      let currentNode = toVisitQueue.shift();
+      if (!currentNode) return sum += currentNode.val;
+
+      sum += currentNode.val;
+      for (let child of currentNode.children) {
+        toVisitQueue.push(child);
+      }
+    }
+    return sum;
   }
+
 
   /** countEvens(): count all of the nodes in the tree with even values. */
+  //Given a n-ary tree of integers, return the count of all the even integers.
+
+  // countEvens(count = 0, rootNode = this.root) {
+  //   if (!rootNode) return count; 
+  //   let currentNode = rootNode.push(rootNode.val + this.countEvens(rootNode.children, count));
+  //   return currentNode;
+  // }
+
+
+
+  // countEvens(count = 0) {
+  //   if (!this.root) return count;
+  //   let toVisitQueue = [this.root];
+  //   while (toVisitQueue.length) {
+  //     let currentNode = toVisitQueue.shift();
+  //     if (!currentNode) continue;
+  //     if (currentNode.val % 2 === 0) count++;
+  //     for (let child of currentNode.children) {
+  //       toVisitQueue.push(child);
+  //     }
+  //   }
+  //   return count;
+  // }
+
 
   countEvens() {
-
+    // base case 
+    if (!this.root) return 0;
+    let count = 0;
+    let toVisitQueue = [this.root];
+    while (toVisitQueue.length) {
+      let currentNode = toVisitQueue.shift();
+      if (!currentNode) continue;
+      if (currentNode.val % 2 === 0) count++;
+      for (let child of currentNode.children) {
+        toVisitQueue.push(child);
+      }
+    }
+    return count;
   }
+
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
+    if (!this.root) return 0;
+    let count = 0;
+    let toVisitQueue = [this.root];
+    while (toVisitQueue.length) {
+      let currentNode = toVisitQueue.shift();
+      if (!currentNode) continue;
+      if (currentNode.val > lowerBound) count++;
+      for (let child of currentNode.children) {
+        toVisitQueue.push(child);
+      }
+    }
+    return count;
 
   }
 }
 
-module.exports = { Tree, TreeNode };
+module.exports = {
+  Tree,
+  TreeNode
+};
+
+emptyTree = new Tree();
+
+// build small tree
+let nSmall = new TreeNode(1);
+let nSmall2 = new TreeNode(2);
+nSmall.children.push(nSmall2);
+smallTree = new Tree(nSmall);
+
+// build large tree
+let n = new TreeNode(1);
+let n2 = new TreeNode(2);
+let n3 = new TreeNode(3);
+let n4 = new TreeNode(4);
+let n5 = new TreeNode(5);
+let n6 = new TreeNode(6);
+let n7 = new TreeNode(7);
+let n8 = new TreeNode(8);
+
+n.children = [n2, n3, n4];
+
+n4.children.push(n5, n6);
+n6.children.push(n7);
+n7.children.push(n8);
+
+largeTree = new Tree(n);
+
+console.log("Sum of smallTree:", smallTree.sumValues()); //3
+console.log("Sum of largeTree:", largeTree.sumValues()); // 36
+console.log("Count of evens in smallTree:", smallTree.countEvens()); //1
+console.log("Count of evens in largeTree:", largeTree.countEvens()); //4
+console.log("Count evens in smallTree:", smallTree.countEvens()); //1
+console.log("Count evens in largeTree:", largeTree.countEvens()); //4
+console.log("Small tree numGreater:", smallTree.numGreater(0)); //2 
+console.log("Large tree numGreater:", largeTree.numGreater(0)); //8
+console.log("Small tree numGreater:", smallTree.numGreater(1)); //1
+console.log("Large tree numGreater:", largeTree.numGreater(1)); //7
